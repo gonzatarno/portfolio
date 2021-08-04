738a3217f088bb0b5b7ba19d9a8b3ee575ba5efd
Gonzalo Tarnofsky <gonzalotarnofsky@gmail.com>
2021-08-04 14:05:28 -0300

    Contact Form

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e)=>{
+  e.preventDefault();
+  statusTxt.style.color = "#22252A";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Enviando mensaje...";
+  form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "message.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("¡Es necesario un Email y un Mensaje!") != -1 || response.indexOf("Ingresá un email valido") != -1 || response.indexOf("Lo siento, no pudimos mandar tu mensaje") != -1){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  }
+  let formData = new FormData(form);
+  xhr.send(formData);
+}
